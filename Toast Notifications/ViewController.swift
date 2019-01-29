65c8ed60d939364087e48a200b01c53922f8d12e
Prashant G <prashant.gaikwad@leftrightmind.com>
2019-01-29 22:03:24 +0530

    toast messages with images and text

--- Toast Notifications/ViewController.swift
+++ Toast Notifications/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Toast_Swift
 
 class ViewController: UIViewController {
 
@@ -15,6 +16,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func showToast(_ sender: Any) {
+        // basic usage
+        self.view.makeToast("This is a piece of toast")
+        
+        // toast with a specific duration and position
+        self.view.makeToast("This is a piece of toast", duration: 3.0, position: .center)
+        
+        // toast presented with multiple options and with a completion closure
+        self.view.makeToast("This is a piece of toast", duration: 4.0, point: CGPoint(x: 200.0, y: UIScreen.main.bounds.width/2), title: "Toast Title", image: UIImage(named: "toast.png")) { didTap in
+            if didTap {
+                print("completion from tap")
+            } else {
+                print("completion without tap")
+            }
+        }
+    }
+    
 
 }
 
